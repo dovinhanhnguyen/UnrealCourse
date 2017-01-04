a0ade6c6b82c50b6b1ff6f215efb407e2dae4ee2
Anh Nguyen <dovinhanh.nguyen@yahoo.co.uk>
2017-01-04 12:34:13 +0700

    Section 2, lecture 18

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -3,20 +3,35 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// The entry point for our application
 int main() 
 {
-	/*Introduce the game*/
-	constexpr int WORD_LENGTH = 5; // const so must assign value immediately
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	return 0;
+}
+
+// Introduce the game
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 5; // const so must assign value immediately; this is only in the scope of this function
 	cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	return; // void function so returns nothing
+}
 
-	/*Get a guess from the player*/
+// Get a guess from the player
+string GetGuessAndPrintBack()
+{
 	string Guess = "";
 	cout << "Enter your guess: ";
 	getline(cin, Guess); // problem with cin: cin reads until a space character then it stops
-
-	/*Return the guess back to them*/
 	cout << "Your guess is " << Guess << endl;
-
-	return 0;
+	return Guess;
 }
