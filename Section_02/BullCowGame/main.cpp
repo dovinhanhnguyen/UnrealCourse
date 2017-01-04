1d7bcb59f3b64bc95eca207b24dd01b2113eb382
Anh Nguyen <dovinhanh.nguyen@yahoo.co.uk>
2017-01-04 12:12:55 +0700

    Section 2, lecture 16

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() 
 {
+	/*Introduce the game*/
 	constexpr int WORD_LENGTH = 5; // const so must assign value immediately
-
 	cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+
+	/*Get a guess from the player*/
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	/*Return the guess back to them*/
+	cout << "Your guess is " << Guess << endl;
 
 	return 0;
 }
